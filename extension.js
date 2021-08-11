86bfe178b6a0eb8e6c346b089940dfa0edc6cef0
xiaoas <xiaoqilin82@gmail.com>
2021-08-11 23:03:07 +0800

    init directory
    add assets

---
+++ extension.js
@@ -0,0 +1,38 @@
+game.import('extension', function (lib, game, ui, get, ai, _status) {
+    return {
+        name: '部将',
+        content: function (config, pack) {
+
+        },
+        precontent: function () {
+
+        },
+        config: {}, help: {},
+        package: {
+            character: {
+                character: {
+                },
+                translate: {
+                },
+            },
+            card: {
+                card: {
+                },
+                translate: {
+                },
+                list: [],
+            },
+            skill: {
+                skill: {
+                },
+                translate: {
+                },
+            },
+            intro: '',
+            author: 'xiaoas',
+            diskURL: '',
+            forumURL: '',
+            version: '0.1',
+        }, files: { character: [], card: [], skill: [] }
+    }
+})
