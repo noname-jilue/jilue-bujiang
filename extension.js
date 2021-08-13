62ed74714ee252ecada5ff2b2626b64a4fae1e6d
xiaoas <xiaoqilin82@gmail.com>
2021-08-13 22:20:13 +0800

    implements

--- extension.js
+++ extension.js
@@ -32,6 +32,7 @@ game.import('extension', function (lib, game, ui, get, ai, _status) {
                 ui.system.classList.remove('blur');
                 ui.menuContainer.classList.remove('blur');
                 evt.currentTarget.delete();
+                delete this.panel;
                 evt.stopPropagation();
                 // if(resume) game.resume2();
                 return false;
@@ -40,8 +41,187 @@ game.import('extension', function (lib, game, ui, get, ai, _status) {
             ui.window.appendChild(layer);
             const mainPanel = ui.create.div('.menubg.jlsgbujiang', layer);
             mainPanel.addEventListener('pointerup', ui.click.touchpop);
+            this.panel = {
+                node: mainPanel,
+            }
+            this.start();
             // lib.setHover(mainPanel, () => {});
-        }
+            // const idb = await import('./modules/index.js');
+        },
+        async save() {
+            if (this._isSaving) {
+
+                return;
+            }
+            localStorage.setItem('bujiang', JSON.stringify(this.data));
+        },
+        start() {
+            this.setupData();
+            if (this.data.states.needInitialGive) {
+                // await Give a random list of orbs
+                /** [
+                 *      Color: 0 purple,
+                 *             1 shu,
+                 *             2 qun,
+                 *             3 wu,
+                 *             4 wei,
+                 *      type:  0 | 1(STR) | 2(AGI) | 3(INT)
+                 * [skillName, count],
+                 * [skillName2, count2] | null,
+                 * ] */
+                let gifts = [
+                    [1, 1, ['rende', 4], ['paoxiao', 3]],
+                    [1, 1, ['rende', 4], ['paoxiao', 3]],
+                    [1, 1, ['rende', 4]],
+                ]
+                this.gainOrbs(gifts);
+                this.data.states.needInitialGive = false;
+                this.save();
+            }
+            if (this.bonusReady) {
+                // alert daily bonus
+            }
+        },
+        /**
+         * give status of the given suit
+         * @param {[[string?, string?, string?],[string?, string?, string?],[string?, string?, string?]]} suit 
+         */
+        report(suit) {
+            if (suit.flat().some(id => id && !this.data.orbs[id])) {
+                return false;
+            }
+            let orbData = [[], [], []];
+            for (let [i, v] of suit.entries()) {
+                for (let [j, w] of v.entries()) {
+                    orbData[i][j] = this.data.orbs[w];
+                }
+            }
+            console.log(orbData);
+            let result = {
+                colors: [0, 0, 0, 0],
+                types: 0,
+                skills: [],
+            }
+            let solve = orbs => {
+                if (orbs.some(o => !o)) {
+                    return;
+                }
+                let color;
+                let skills = {};
+                for (o of orbs) {
+                    // color
+                    if (o[0] && color !== false) {
+                        if (color && color !== o[0]) {
+                            color = false;
+                        } else {
+                            color = o[0];
+                        }
+                    }
+                    // skill
+                    let addSkill = ([name, cnt]) => {
+                        skills[name] = skills[name] || 0;
+                        skills[name] += cnt;
+                    };
+                    addSkill(o[2]);
+                    if (o[3]) {
+                        addSkill(o[3]);
+                    }
+                }
+                if (color) {
+                    ++result.colors[color - 1];
+                }
+                if (orbs[0][1] && orbs.every(o => o[1] === orbs[0][1])) {
+                    result.types = orbs[0][1];
+                }
+                for (let s in skills) {
+                    if (skills[s] >= 10) {
+                        result.skills.push(s);
+                    }
+                }
+            };
+            for (let i of [0, 1, 2]) {
+                solve(orbData[i]);
+                solve([orbData[0][i],orbData[1][i],orbData[2][i]]);
+            }
+            solve([orbData[0][0], orbData[1][1],orbData[2][2]]);
+            solve([orbData[0][2], orbData[1][1],orbData[2][0]]);
+            /** FIXME: implement color requirements */
+            return result;
+        },
+        get bonusReady() {
+            /** FIXME: move to daily bonus module as sub function */
+            this.setupData();
+            if (!this.data.bonusDay) {
+                return true;
+            }
+            let lastDay = new Date(this.data.bonusDay);
+            let tday = new Date();
+            tday = Date.UTC(tday.getFullYear(), tday.getMonth(), tday.getDate());
+            lastDay = Date.UTC(lastDay.getFullYear(), lastDay.getMonth(), lastDay.getDate());
+            return tday > lastDay;
+        },
+        getBonus() {
+            // TODO: fix, move to sub module
+            this.data.bonusDay = Date.now();
+            this.save();
+        },
+        gainOrbs(orbs) {
+            if (Array.isArray(orbs)) {
+                var ids = [];
+                for (let orb of orbs) {
+                    // orb = {...orb};
+                    let orbID = this.makeid(5, true);
+                    ids.push(orbID);
+                    this.data.orbs[orbID] = orb;
+                }
+            } else {
+                ids = Object.keys(orbs);
+                Object.assign(this.data.orbs, orbs);
+            }
+            this.orbList && this.orbList.gainOrbs(ids);
+            this.save();
+        },
+        setupData() {
+            if (this.data) {
+                return;
+            }
+            let data = localStorage.getItem('bujiang')
+            if (data) {
+                this.data = JSON.parse(data)
+            }
+            if (!this.data || this.data.cheater) {
+                this.data = {
+                    suits: [
+                        {
+                            name: '默认',
+                            orbs: [
+                                [null, null, null],
+                                [null, null, null],
+                                [null, null, null],
+                            ],
+                        }
+                    ],
+                    orbs: {},
+                    states: {
+                        needInitialGive: true,
+                    }
+                }
+                this.save();
+            }
+        },
+        makeid(length, noDup = false) {
+            var result = '';
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var charactersLength = characters.length;
+            for (var i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() *
+                    charactersLength));
+            }
+            // if (noDup && this.data,orbs[result]) {
+            //     return this.makeid(length, true);
+            // }
+            return result;
+        },
     };
     return {
         name: '部将',
@@ -74,6 +254,8 @@ game.import('extension', function (lib, game, ui, get, ai, _status) {
                     });
                 })
             }
+            // debug
+            window.bujiangI = internals;
         },
         precontent: function (config) {
             if (!config.enable) {
